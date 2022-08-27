1b59bbf4a0a24941d1e89edcba914fa47eff7ba8
Desor Guishny <desorguishny@gmail.com>
2022-08-27 11:26:11 -0400

    Add my_list file

--- my_list.rb
+++ my_list.rb
@@ -1,2 +1,16 @@
-# my_list.rb
+require_relative 'my_enumerable'
 
+class MyList
+  include MyEnumerable
+  def initialize(*args)
+    @list = *args
+  end
+
+  def to_s
+    @list.join(', ')
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
